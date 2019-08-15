ac3db59ba67e2ea75e72f5ec8ca624ed6ba82fe9
lucianomarqueto@gmail.com <m4rqu3t0>
2019-08-15 13:16:29 -0300

    flask basico

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,44 @@
-from __future__ import print_function
+# from __future__ import print_function
 
-import os
-import json
+# import os
+# import json
 
-from flask import Flask, request
-from flask_restful import Resource, Api
+# from flask import Flask, request
+# from flask_restful import Resource, Api
 
-application = Flask(__name__)
+# application = Flask(__name__)
 
-api = Api(application)
+# api = Api(application)
 
-class Hello(Resource):
-    def get(self):
-        return 'Hello World'
+# class Hello(Resource):
+#     def get(self):
+#         return 'Hello World'
 
-api.add_resource(Hello, '/ws/hello/')
+# api.add_resource(Hello, '/ws/hello/')
 
-@application.route('/')
-def index():
-    return 'Welcome to the National Parks data service.'
+# @application.route('/')
+# def index():
+#     return 'Welcome to the National Parks data service.'
+
+# if __name__ == '__main__':
+#     app.run(debug=False, host='0.0.0.0')
+
+# load Flask 
+import flask
+app = flask.Flask(__name__)
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {"success": False}
+    # get the request parameters
+    params = flask.request.json
+    if (params == None):
+        params = flask.request.args
+    # if parameters are found, echo the msg parameter 
+    if (params != None):
+        data["response"] = params.get("msg")
+        data["success"] = True
+    # return a response in json format 
+    return flask.jsonify(data)
+# start the flask app, allow remote connections
+app.run(host='0.0.0.0')
