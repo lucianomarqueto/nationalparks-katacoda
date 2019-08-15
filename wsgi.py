d78841206ff892f5e46288cacc6dc069f3ff933c
lucianomarqueto@gmail.com <m4rqu3t0>
2019-08-15 16:24:08 -0300

    os

--- wsgi.py
+++ wsgi.py
@@ -1,58 +1,11 @@
-''' from __future__ import print_function
-
-import os
-import json
-
-from flask import Flask, request
-from flask_restful import Resource, Api
-
-application = Flask(__name__)
-
-api = Api(application)
-
-class Hello(Resource):
-    def get(self):
-        return 'Hello World'
-
-api.add_resource(Hello, '/ws/hello/')
-
-@application.route('/')
-def index():
-    return 'Welcome!!!.'
-
-if __name__ == '__main__':
-    application.run(debug=False, host='0.0.0.0')
- '''
-''' 
-import flask
-application = flask.Flask(__name__)
-# define a predict function as an endpoint 
-@application.route("/predict", methods=["GET","POST"])
-def predict():
-    data = {"success": False}
-    # get the request parameters
-    params = flask.request.json
-    if (params == None):
-        params = flask.request.args
-    # if parameters are found, echo the msg parameter 
-    if (params != None):
-        data["response"] = params.get("msg")
-        data["success"] = True
-    # return a response in json format 
-    return flask.jsonify(data)
-
-@application.route('/')
-def index():
-    return 'Welcome back!!!.'
-
-# start the flask app, allow remote connections
-if __name__ == '__main__':  
-    application.run(host='0.0.0.0')
- '''
-
 
 import flask
+
 #import pandas as pd
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 import tensorflow as tf
 #import keras
 #from keras.models import load_model
